3fb3dea6dee71b01d4e768fd2ad2b3fa6e2393aa
mvicenzi <matteo.vicenzi7@gmail.com>
2021-12-06 10:11:54 +0100

    update i/o using cvs files

--- update_broken_sipm.py
+++ update_broken_sipm.py
@@ -26,13 +26,14 @@ def find_scintillator(crt_barcode,bar_number):
 
     bar = "scin" + str(bar_number)
     scin_barcode = dataQuery.query(database=dBname, table=crtTable, columns=bar,where='crt_barcode:eq:'+crt_barcode)
-    print(scin_barcode)
     return scin_barcode[0]
 
 # get full scintillator data from scintillator table
 def get_scintillator_data(scin_barcode):
 
-    scin = dataQuery.query(database=dBname, table=scinTable, columns='*', where='scin_barcode:eq:'+scin_barcode)
+    scin = dataQuery.query(database=dBname, table=scinTable,
+                           columns='scin_barcode,scin_prod,scin_sipm_r,scin_sipm_l,scin_test,light_y,attenu_t,attenu_l',
+                           where='scin_barcode:eq:'+scin_barcode)
     return scin[0].split(",")
 
 # check if sipm exists in sipm table
@@ -41,6 +42,7 @@ def is_sipm_in_table(serial_number):
     result = dataQuery.query(database=dBname, table=sipmTable, columns="*", where="serial_num:eq:"+str(serial_number))
     if len(result)>1:
         return True
+    print(serial_number, "is not in topcrt_sipm table")
     return False
 
 # convert channel to bar number
@@ -65,20 +67,38 @@ def update_sipm_in_scintillator(scintillator, channel, new_sipm):
 #----------------------------------------------------------------#
 def main():
 
-    #test example
-    crt_barcode = "CRT00125"
-    channel = 1
-    new_sipm = 14324
+    #open input file
+    infile = open(sys.argv[1], "r")
 
-    bar = get_bar_from_channel(channel)
-    scin_barcode = find_scintillator("CRT00125",bar)
-    scintillator = get_scintillator_data(scin_barcode)
+    #Take the dir path of the input file
+    pathfile = os.path.abspath(sys.argv[1])
+    dirpath, filename = os.path.split(pathfile)
+    outpath = os.path.join(dirpath,"updated_scin_entries.csv")
 
-    print(scintillator)
+    # open output file
+    outfile = open(outpath ,"w")
 
-    if is_sipm_in_table(new_sipm):
-        update_sipm_in_scintillator(scintillator, channel, new_sipm)
-    print(scintillator)
+    reader = csv.reader(infile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
+
+    # write header in output file
+    first_row = "scin_barcode,scin_prod,scin_sipm_r,scin_sipm_l,scin_test,light_y,attenu_t,attenu_l"
+    first_row = first_row.split(",")
+    writer.writerow(first_row)
+
+    # iterate over all lines in input file
+    for row in reader:
+        crt_barcode = row[0]
+        channel = int(row[1])
+        new_sipm = int(row[2])
+
+        bar = get_bar_from_channel(channel)
+        scin_barcode = find_scintillator(crt_barcode,bar)
+        scintillator = get_scintillator_data(scin_barcode)
+
+        if is_sipm_in_table(new_sipm):
+            update_sipm_in_scintillator(scintillator, channel, new_sipm)
+            writer.writerow(scintillator)
 
 if __name__ == "__main__":
     main()
