3fd622aeee2e4634abd07e76f29aecf90fa1e39d
Matteo Vicenzi <64699641+mvicenzi@users.noreply.github.com>
2022-05-04 00:42:46 +0200

    added primary, unique, foreign keys to dump -> completed

--- generate_SQLite_dump.py
+++ generate_SQLite_dump.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 #---------------------------------------------------------------
 # This script generates a SQLite dump of top CRT tables,
-# using info from ICARUS hardware database.
+# using info from ICARUS hardware database. It is basically a manual
+# conversion from PostGreSQL to SQLite.
 #
 # Author: mvicenzi
 # Inspired by: copyPostGresToSQLite.py (by T. Usher)
@@ -16,10 +17,10 @@ import re #regex
 from   DataLoader3 import DataLoader, DataQuery
 
 # Set up the columns for the tables
-topcrtFEBColumns = ['feb_barcode text',
-                    'serialnum integer',
-                    'mac_add8b text',
-                    'mac_add integer',
+topcrtFEBColumns = ['feb_barcode text PRIMARY KEY NOT NULL',
+                    'serialnum integer DEFAULT NULL UNIQUE',
+                    'mac_add8b text DEFAULT NULL UNIQUE',
+                    'mac_add integer DEFAULT NULL UNIQUE',
                     'voltage float',
                     'feb_index integer',
                     'ch0 integer',
@@ -59,19 +60,19 @@ topcrtFEBColumns = ['feb_barcode text',
                     'update_time text',
                     'create_user text']
 
-topcrtSiPMColumns = ['serial_num integer',
-                     'voltage float',
-                     'dark_curr float',
+topcrtSiPMColumns = ['serial_num integer PRIMARY KEY NOT NULL',
+                     'voltage float NOT NULL',
+                     'dark_curr float NOT NULL',
                      'comment text',
                      'create_time text',
                      'update_user text',
                      'update_time text',
                      'create_user text']
 
-topcrtSCINTILLATORColumns = ['scin_barcode text',
+topcrtSCINTILLATORColumns = ['scin_barcode text PRIMARY KEY NOT NULL',
                              'scin_prod text',
-                             'scin_sipm_r integer',
-                             'scin_sipm_l integer',
+                             'scin_sipm_r integer DEFAULT NULL UNIQUE',
+                             'scin_sipm_l integer DEFAULT NULL UNIQUE',
                              'scin_test text',
                              'light_y float',
                              'attenu_t float',
@@ -81,24 +82,24 @@ topcrtSCINTILLATORColumns = ['scin_barcode text',
                              'update_time text',
                              'create_user text']
 
-topcrtMODULEColumns = ['crt_barcode text',
-                       'feb_barcode text',
-                       'scin1 text',
-                       'scin2 text',
-                       'scin3 text',
-                       'scin4 text',
-                       'scin5 text',
-                       'scin6 text',
-                       'scin7 text',
-                       'scin8 text',
-                       'scin9 text',
-                       'scin10 text',
-                       'scin11 text',
-                       'scin12 text',
-                       'scin13 text',
-                       'scin14 text',
-                       'scin15 text',
-                       'scin16 text',
+topcrtMODULEColumns = ['crt_barcode text PRIMARY KEY NOT NULL',
+                       'feb_barcode text DEFAULT NULL UNIQUE',
+                       'scin1 text DEFAULT NULL UNIQUE',
+                       'scin2 text DEFAULT NULL UNIQUE',
+                       'scin3 text DEFAULT NULL UNIQUE',
+                       'scin4 text DEFAULT NULL UNIQUE',
+                       'scin5 text DEFAULT NULL UNIQUE',
+                       'scin6 text DEFAULT NULL UNIQUE',
+                       'scin7 text DEFAULT NULL UNIQUE',
+                       'scin8 text DEFAULT NULL UNIQUE',
+                       'scin9 text DEFAULT NULL UNIQUE',
+                       'scin10 text DEFAULT NULL UNIQUE',
+                       'scin11 text DEFAULT NULL UNIQUE',
+                       'scin12 text DEFAULT NULL UNIQUE',
+                       'scin13 text DEFAULT NULL UNIQUE',
+                       'scin14 text DEFAULT NULL UNIQUE',
+                       'scin15 text DEFAULT NULL UNIQUE',
+                       'scin16 text DEFAULT NULL UNIQUE',
                        'test text',
                        'created_by text',
                        'produced text',
@@ -111,7 +112,7 @@ topcrtMODULEColumns = ['crt_barcode text',
                        'update_time text',
                        'create_user text']
 
-topcrtCABLEColumns = ['cable_id text',
+topcrtCABLEColumns = ['cable_id text PRIMARY KEY NOT NULL',
                       'cable_type text',
                       'cable_function text',
                       'cable_length float',
@@ -122,18 +123,18 @@ topcrtCABLEColumns = ['cable_id text',
                       'update_time text',
                       'create_user text']
 
-topcrtPOWERColumns = ['cable_id text',
+topcrtPOWERColumns = ['cable_id text PRIMARY KEY NOT NULL',
                       'powerline int',
                       'plus_lines text',
                       'sensing text',
-                      'feb1 text',
-                      'feb2 text',
+                      'feb1 text NULL UNIQUE',
+                      'feb2 text NULL UNIQUE',
                       'create_time text',
                       'update_user text',
                       'update_time text',
                       'create_user text']
 
-topcrtPOSITIONColumns = ['crt_barcode text',
+topcrtPOSITIONColumns = ['crt_barcode text PRIMARY KEY',
                          'wall text',
                          'row_index integer',
                          'column_index integer',
@@ -145,6 +146,47 @@ topcrtPOSITIONColumns = ['crt_barcode text',
                          'update_time text',
                          'create_user text']
 
+################################################################################
+
+def setForeignKeys(table):
+
+    keysString = ""
+    if table == "crtmodule":
+        keysString += ", FOREIGN KEY (feb_barcode) REFERENCES crtfeb(feb_barcode), "
+        keysString += "FOREIGN KEY (scin1) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin2) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin3) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin4) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin5) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin6) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin7) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin8) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin9) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin10) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin11) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin12) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin13) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin14) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin15) REFERENCES scintillator(scin_barcode), "
+        keysString += "FOREIGN KEY (scin16) REFERENCES scintillator(scin_barcode)"
+
+    if table == "scintillator":
+        keysString += ", FOREIGN KEY (scin_sipm_r) REFERENCES topcrt_sipm(serial_num), "
+        keysString += "FOREIGN KEY (scin_sipm_l) REFERENCES topcrt_sipm(serial_num)"
+
+    if table == "crtcable":
+        keysString += ", FOREIGN KEY (start_feb) REFERENCES crtfeb(feb_barcode), "
+        keysString += "FOREIGN KEY (end_feb) REFERENCES crtfeb(feb_barcode)"
+
+    if table == "crtpower":
+        keysString += ", FOREIGN KEY (feb1) REFERENCES crtfeb(feb_barcode), "
+        keysString += "FOREIGN KEY (feb2) REFERENCES crtfeb(feb_barcode)"
+
+    if table == "crtposition":
+        keysString += ", FOREIGN KEY (crt_barcode) REFERENCES crtmodule(crt_barcode)"
+
+    return keysString
+
 # Define the function to create and fill each table
 def copyTable(postGres, dbCurs, dbName, table, columns):
     createString = "CREATE TABLE " + table + " ("
@@ -154,11 +196,11 @@ def copyTable(postGres, dbCurs, dbName, table, columns):
             createString += ", "
         createString += columns[columnIdx]
 
+    createString += setForeignKeys(table)
     createString += ")"
 
     print(createString)
     dbCurs.execute(createString)
-
     query = postGres.query(database=dbName, table=table, columns="*")
 
     lastIdx = len(query)-1;
@@ -182,7 +224,12 @@ def copyTable(postGres, dbCurs, dbName, table, columns):
                 insertString += ", "
             if "text" in columns[idx]:
                 fieldEntry = rowList[idx]
-                insertString += "\'" + fieldEntry + "\'"
+                # changing "None" into NULL is necessary for compliance
+                # with UNIQUE statements in certain columns (eg: feb2 in crtpower)
+                if fieldEntry == "None":
+                    insertString += "NULL"
+                else:
+                    insertString += "\'" + fieldEntry + "\'"
             else:
                 if "none" in rowList[idx]:
                     #print("Found none in column data, field:",columns[idx],", value: ",rowList[idx])
@@ -209,7 +256,10 @@ topcrtpositionTable = "crtposition"
 ##################################################################################
 
 # Start by creating the new database and setting up the first table
-sqliteDB = sqlite3.connect("test.db")
+today = str(datetime.date.today())
+dump_name = "topCRT_hw_dump_" + today + ".db"
+
+sqliteDB = sqlite3.connect(dump_name)
 dbCurs   = sqliteDB.cursor()
 
 ###################################################################################
