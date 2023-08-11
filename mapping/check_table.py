3019f58638b10be30eca01da617fa90a982769d9
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2023-08-11 09:09:15 -0500

    improvements by G. Petrillo

--- mapping/check_table.py
+++ mapping/check_table.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import sqlite3
 import csv
 import datetime
+import sys
 
 
 def print_table_info(database_path):
@@ -27,6 +30,7 @@ def print_table_info(database_path):
 
     # Close the database connection
     conn.close()
+    return 0
 
 #### ---------------------------------------
 #### ---------------------------------------
@@ -37,7 +41,7 @@ def print_table_contents(db_path, table_name):
     cursor = conn.cursor()
 
     # Retrieve all rows from the specified table
-    cursor.execute(f"SELECT * FROM {table_name};")
+    cursor.execute(f"SELECT * FROM {table_name} ORDER BY pmt_id;")
     rows = cursor.fetchall()
 
     # Print the column headers
@@ -46,21 +50,59 @@ def print_table_contents(db_path, table_name):
     column_names = [column[1] for column in columns]
     print(', '.join(column_names))
 
-    # Print the table contents
+    if len(rows) == 0:
+        print("No data.")
+        return
+    
+    padding = [ 0 ] * len(rows[0])
     for row in rows:
-        print(', '.join(map(str, row)))
+        for iCol, value in enumerate(row):
+            padding[iCol] = max(padding[iCol], len(str(value)))
+    
+    uniqueColumns = (
+        'pmt_id', 'pmt_sn', 'hv_cable_label', 'signal_cable_label',
+        'channel_id',
+        )
+    uniqueColumnIndices = list(map(column_names.index, uniqueColumns))
+    dupCheckValues = [ dict() for i in range(len(uniqueColumns)) ]
+    print(f"{uniqueColumnIndices=}")
 
+    nDups = 0
+    # Print the table contents; also check for duplicates
+    for iRow, row in enumerate(rows):
+        print(', '.join(map(lambda p: f"{str(p[1]):>{padding[p[0]]}s}", enumerate(row))))
+        for iCheck, iCol in enumerate(uniqueColumnIndices):
+            if row[iCol] in dupCheckValues[iCheck]:
+                print(f"{column_names[iCol]}='{row[iCol]}' on row {iRow}"
+                      f" already found on row {dupCheckValues[iCheck][row[iCol]]}",
+                      file=sys.stderr)
+                nDups += 1
+            else: dupCheckValues[iCheck][row[iCol]] = iRow
+        # for values
+    # for rows
+    
     # Close the connection
     conn.close()
+    
+    if nDups > 0:
+        print(f"Found {nDups} duplicate elements!", file=sys.stderr)
+        return 1
+    
+    return 0
 
 #### --------------------------------------
 #### --------------------------------------
+
+nErrors = 0
 
 db_path = 'ChannelMapICARUS_August2023.db'
-print_table_info(db_path)
+nErrors += print_table_info(db_path)
 
 old_table_name = 'pmt_placements'
-print_table_contents(db_path, old_table_name)
+nErrors += print_table_contents(db_path, old_table_name)
 
 new_table_name = 'pmt_placements_Aug2023'
-print_table_contents(db_path, new_table_name)
+nErrors += print_table_contents(db_path, new_table_name)
+
+if nErrors > 0:
+    raise RuntimeError(f"{nErrors} checks failed!!")
